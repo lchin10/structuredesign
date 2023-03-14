7bb472d9150739c41ae4edfa4970a7d863ce98b3
Lukas Chin <lchin10@bu.edu>
2023-03-14 22:46:07 +1100

    part 1 design

--- design_script.js
+++ design_script.js
@@ -1,5 +1,6 @@
 const sec1 = document.getElementById('section1')
 const sec2 = document.getElementById('section2')
+const sec3 = document.getElementById('section3')
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
@@ -12,3 +13,14 @@ function scrollToSection1() {
 function scrollToSection2() {
     sec2.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToSection3() {
+    sec3.scrollIntoView({ behavior: "smooth" });
+}
+
+const numberSlider = document.getElementById("number-slider");
+const selectedNumber = document.getElementById("selected-number");
+
+numberSlider.addEventListener("input", function() {
+    selectedNumber.innerHTML = numberSlider.value;
+});
